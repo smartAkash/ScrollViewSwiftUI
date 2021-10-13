b9d094844e13d7a0d50e9cbfa7dd202acecc573d
Akash <akash.nara@smartsensesolutions.com>
2021-10-13 14:35:07 +0530

    Initial Commit

---
+++ ScrollViewSwiftUI/ScrollViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewSwiftUIApp.swift
+//  ScrollViewSwiftUI
+//
+//  Created by Akash on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
